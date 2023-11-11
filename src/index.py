c85ae9c71ffabfeb0d216d6d9802a8bc2f4725e0
xcvbnmas <132822281+xcvbnmas@users.noreply.github.com>
2023-11-11 16:34:05 +0200

    viikko2 4

--- src/index.py
+++ src/index.py
@@ -18,7 +18,7 @@ def main():
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
     print(f"Mehuvarasto: {mehua}")
-    print("Otetaan 3.14 pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp")
+    print("Otetaan 3.14")
 
 if __name__ == "__main__":
     main()
